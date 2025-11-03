40253a15e93fbc51ee4b812bb9b07758c7a8b1b9
Jhon Anthony Varron <jhonanthonyvarron02@gmail.com>
2025-11-03 21:53:03 +0800

    Adds basic sidebar structure and navigation
    Implements a basic sidebar with a logo, header, and navigation links.
    
    This provides a consistent navigation structure for the application, allowing
    users to easily access different sections like the dashboard and feedback
    page. Styles the sidebar for improved visual appeal.
    
    The sidebar is now a sticky element, ensuring it remains visible during scrolling.

--- src/app/components/app-sidebar.tsx
+++ src/app/components/app-sidebar.tsx
@@ -1,24 +1,36 @@
-'use client';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider, useSidebar } from "@/components/ui/sidebar";
-import { Calendar, Home, Inbox, Search, Settings, } from "lucide-react";
+import { Home, MessageSquare } from "lucide-react";
 
+type Props = { className?: string };
 
-export function AppSidebar() {
-  // const { state } = useSidebar();
-
+export function AppSidebar({ className }: Props) {
   const items = [
     {
       title: 'Dashboard',
-      url: '#',
+      url: '/',
       icon: Home,
     },
+    {
+      title: 'Feedback',
+      url: '/feedback',
+      icon: MessageSquare,
+    }
   ]
 
   return (
-    <Sidebar side="left" variant="sidebar" collapsible="offcanvas">
-       <SidebarContent>
+    <aside className={`sticky top-[7rem] h-[calc(100vh-7rem)] overflow-auto ${className ?? "" }`}>
+      <Sidebar side="left" variant="sidebar" collapsible="offcanvas">
+        <SidebarHeader className="border-b p-4"> 
+          <div className="flex items-center gap-2"> 
+            <div className="bg-blue-600 text-white w-8 h-8 rounded flex items-center justify-center font-bold">
+              Logo
+            </div>
+            <h4 className="font-semibold text-lg">ResMate</h4>
+          </div>
+        </SidebarHeader>
+
+        <SidebarContent>
           <SidebarGroup>
-            <SidebarGroupLabel>Application</SidebarGroupLabel>
             <SidebarGroupContent>
               <SidebarMenu>
                 {items.map((item) => (
@@ -34,7 +46,8 @@ export function AppSidebar() {
               </SidebarMenu>
             </SidebarGroupContent>
           </SidebarGroup>
-        </SidebarContent>
-    </Sidebar>
+        </SidebarContent> 
+      </Sidebar>
+    </aside>
   );
 }

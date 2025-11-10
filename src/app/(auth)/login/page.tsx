0d588895e30ff15bde56bc193a90e8b4e496da8b
Jhon Anthony Varron <jhonanthonyvarron02@gmail.com>
2025-11-10 11:40:34 +0800

    Simplifies the login page component.
    Removes unused imports and logic from the login page
    component, focusing on rendering the login form. This
    streamlines the component and improves readability.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,19 +1,10 @@
 "use client";
-import { SignupForm } from "@/components/signup-form";
-import { LoginForm } from "@/components/login-form";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 
+import { LoginForm } from "@/components/login-form";
 import { useRouter } from "next/navigation";
-import { FormEvent, use } from "react";
 
 export default function LoginPage() {
   const router = useRouter();
-
-  const handleLogin = (e: FormEvent) => {
-    e.preventDefault();
-    router.push("/");
-  }
 
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">

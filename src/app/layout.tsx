e04d223f2926b20ea614ea4a1e24c01cd9eae16d
Jhon Anthony Varron <jhonanthonyvarron02@gmail.com>
2025-11-22 01:11:38 +0800

    Improves app layout with sidebar and header
    Enhances the application layout by integrating a sidebar and header.
    
    This provides a more structured and user-friendly interface. It includes the AppSidebar for navigation and SiteHeader for branding and potentially user-related actions. The layout is structured using flexbox for responsive and adaptable content arrangement.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 'use client';
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import "./globals.css";
 import { usePathname } from "next/navigation";
 import { AppSidebar } from "./components/app-sidebar";
+import { SiteHeader } from "./components/site-header";
+import App from "next/app";
 
 export default function RootLayout({
   children,
@@ -10,6 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
+
+  const isFeedbackPage = pathname.startsWith("/feedback");
 
   const isAuthRoute = pathname.startsWith("/login") || pathname.startsWith("/register") || pathname.startsWith("/setup") || pathname.startsWith("/otp");
 
@@ -27,16 +31,18 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-amber-100">
         <SidebarProvider>
-          <div className="min-h-screen grid grid-cols-[16rem_1fr] grid-rows-[auto_1fr]">
-            
-            <aside className="row-start-2 col-start-1">
-              <AppSidebar />
-            </aside>
+          <AppSidebar />
+          <SidebarInset>
+            <SiteHeader />
 
-            <main className="row-start-2 col-start-2">
-              {children}
-            </main>
-          </div>
+            <div className="flex flex-1 flex-col">
+              <div className="@container/main flex flex-1 flex-col gap-2">
+                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                  {children}
+                </div>
+              </div>
+            </div>
+          </SidebarInset>
         </SidebarProvider>
       </body>
     </html>

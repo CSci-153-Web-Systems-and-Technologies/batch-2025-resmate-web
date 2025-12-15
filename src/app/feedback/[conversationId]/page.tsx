8c82664d64aa9ff25b3bbdbcd14a9757bce24194
Jhon Anthony Varron <jhonanthonyvarron02@gmail.com>
2025-12-15 12:58:25 +0800

    Init Feedback Page dynamic segment

--- src/app/feedback/[conversationId]/page.tsx
+++ src/app/feedback/[conversationId]/page.tsx
@@ -1,6 +1,4 @@
-import { loadConversationBundle } from "@/lib/db/message-db";
-import FeedbackClient from "../components/feedback-client";
-import { getCurrentUser } from "@/lib/auth/actions/auth";
+import FeedbackPage from "../page";
 
 export default async function FeedbackConversationPage(props: { params: Promise<{ conversationId: string }> }) {
   const params = await props.params;

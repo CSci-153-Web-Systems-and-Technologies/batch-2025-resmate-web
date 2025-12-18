d09b7f976dc6ede0b1ab15c74112edcd5d3d22c9
Jhon Anthony Varron <jhonanthonyvarron02@gmail.com>
2025-12-18 18:58:38 +0800

    Remove unnecessary imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,5 @@
 'use client';
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import "./globals.css";
-import { usePathname } from "next/navigation";
-import { AppSidebar } from "./components/app-sidebar";
-import { SiteHeader } from "./components/site-header";
 
 export default function RootLayout({
   children,
